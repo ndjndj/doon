17b57c618c5dfde7fe716daf10b1661e57f52f89
ndjndj <teq.ndj@gmail.com>
2023-11-03 23:19:28 +0900

    f

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:doon/config/config.dart';
+import 'package:flutter/material.dart';
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Config.title,
+    );
+  }
+}
