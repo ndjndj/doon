8db07b486ff0c74ee8405089777ce6e2160d9898
ndjndj <teq.ndj@gmail.com>
2023-11-05 11:34:06 +0900

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 
 import 'package:doon/internal/config/config.dart';
+import 'package:doon/internal/widget/restart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    const Restart(
+      child: ProviderScope(
+        child: Main()
+      ),
+    )
+  );
+}
 
 class Main extends StatelessWidget {
   const Main({super.key});
