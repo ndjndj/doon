ca474f687ff2321fa1530b239c0fa6052555b5e9
ndjndj <teq.ndj@gmail.com>
2023-11-13 00:01:58 +0900

    f

--- lib/view/util/widget/square_button/square_button.dart
+++ lib/view/util/widget/square_button/square_button.dart
@@ -1,13 +1,23 @@
+import 'package:doon/internal/schema/exports_schema.dart';
 import 'package:flutter/material.dart';
 
-//class SquareButton extends StatelessWidget { 
-//  const SquareButton({
-//    super.key,
-//    this.title,
-//    this.subTitle,
-//    
-//  });
-//
-//  @override
-//  Widget 
-//}
+class SquareButton extends StatelessWidget { 
+  const SquareButton({
+    super.key,
+    required this.quiz
+  });
+
+  final QuizSchema quiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(4)
+        ),
+      ),
+    );
+  }
+}
