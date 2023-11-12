dcb885ceb9d8aa43e8badadc810d18bf35b5b3e3
ndjndj <teq.ndj@gmail.com>
2023-11-12 23:13:22 +0900

    f

--- lib/view/util/widget/scaffold_frame/scaffold_frame.dart
+++ lib/view/util/widget/scaffold_frame/scaffold_frame.dart
@@ -1,4 +1,5 @@
 import 'package:doon/internal/config/config.dart';
+import 'package:doon/view/util/repository/view_state_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,14 +11,13 @@ class ScaffoldFrame extends ConsumerWidget {
 
   @override 
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewState = ref.watch(viewStateProvider);
     return KeyboardDismissOnTap(
       child: DecoratedBox(
         decoration: Config.background,
         child: Scaffold(
           backgroundColor: const Color(0xFFFFFFFF).withOpacity(0.4),
-          body: Center(
-            child: Text("text"),
-          )
+          body: viewState.widget
         )
       )
     );
