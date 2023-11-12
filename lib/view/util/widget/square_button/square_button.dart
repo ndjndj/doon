ec11682355cc6f584a13df27f9bb794e5ad8a89f
ndjndj <teq.ndj@gmail.com>
2023-11-12 23:18:30 +0900

    f

--- lib/view/util/widget/square_button/square_button.dart
+++ lib/view/util/widget/square_button/square_button.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SquareButton extends StatelessWidget { 
-  const SquareButton({
-    super.key,
-    this.title,
-    this.subTitle,
-    
-  });
-
-  @override
-  Widget 
-}
+//class SquareButton extends StatelessWidget { 
+//  const SquareButton({
+//    super.key,
+//    this.title,
+//    this.subTitle,
+//    
+//  });
+//
+//  @override
+//  Widget 
+//}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import device_info_plus
 import flutter_image_compress_macos
+import isar_flutter_libs
 import package_info_plus
 import path_provider_foundation
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
