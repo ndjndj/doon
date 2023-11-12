1c02dbebd0556eb6c43dd4000244254ec2ac047f
ndjndj <teq.ndj@gmail.com>
2023-11-12 23:17:12 +0900

    f

---
+++ lib/view/util/widget/square_button/square_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SquareButton extends StatelessWidget { 
+  const SquareButton({
+    super.key,
+    this.title,
+    this.subTitle,
+    
+  });
+
+  @override
+  Widget 
+}
