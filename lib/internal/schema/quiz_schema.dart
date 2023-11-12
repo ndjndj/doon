f807d63614a99dad1869899ff420ea389e7a2473
ndjndj <teq.ndj@gmail.com>
2023-11-12 23:59:02 +0900

    f

--- lib/internal/schema/quiz_schema.dart
+++ lib/internal/schema/quiz_schema.dart
@@ -3,14 +3,30 @@ import "package:doon/internal/schema/exports_schema.dart";
 import "package:doon/internal/schema/schema_base.dart";
 
 part "quiz_schema.g.dart";
-
+/// QuizSchema
+///
+/// - rankId 
+/// 
+/// ex.) beginner
+///
+/// - rankName 
+/// 
+/// ex.) 初級
+///
+/// - stageId
+///
+/// ex.) 1-1
+///
+/// - stageName
+/// 
+/// ex.) 1-1
 @Collection()
 class QuizSchema {
   Id? id = Isar.autoIncrement;
 
   late String rankId;
 
-  late String rankName;
+  late String rankName; 
 
   late String stageId;
 
