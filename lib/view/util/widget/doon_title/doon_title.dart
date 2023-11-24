f6d763fd2c44c1c338c8300a063926ee3950a81f
ndjndj <teq.ndj@gmail.com>
2023-11-24 18:02:53 +0900

    f

---
+++ lib/view/util/widget/doon_title/doon_title.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DoonTitle extends StatelessWidget {
+  const DoonTitle();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Text("Doon");
+  }
+}
