34a58e4807d9d0dd26dc799c59c9f2f64b664453
ndjndj <teq.ndj@gmail.com>
2023-11-25 22:13:13 +0900

    f

---
+++ lib/view/util/widget/doon_dialog/doon_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class DoonDialog extends StatelessWidget {
+  const DoonDialog();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
