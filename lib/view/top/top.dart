fb2d60611aca6d4924608c00e4e9aeefcd7349d8
ndjndj <teq.ndj@gmail.com>
2023-11-26 19:28:36 +0900

    f

---
+++ lib/view/top/top.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Top extends StatelessWidget {
+  const Top();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Center(child: Text("Top"),);
+  }
+}
