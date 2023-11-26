e4c8dfd0ed3f4f8c172549e508839de7ce890f2f
ndjndj <teq.ndj@gmail.com>
2023-11-26 19:26:43 +0900

    f

---
+++ lib/view/quiz/quiz.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatelessWidget {
+  const Quiz();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Center(child: Text("Quiz"),);
+  }
+}
