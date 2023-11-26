b43707fb9afc59f6f2f8b2849c87c8113c5241b9
ndjndj <teq.ndj@gmail.com>
2023-11-26 19:27:19 +0900

    f

---
+++ lib/view/result/result.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  const Result();
+
+  @override 
+  Widget build(BuildContext context) {
+    return Center(child: Text("Result"),);
+  }
+}
